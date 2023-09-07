23d7c6a509345fc15f5a01aded489762f382d42b
nadazcx <zcnadaa@gmail.com>
2023-09-07 12:37:40 +0100

    fixing the routing and adding home pageé

--- CookingBook/src/app/app.module.ts
+++ CookingBook/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { RecipeComponent } from './recipe/recipe.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
+    HomeComponent,
     AppComponent,
     HeaderComponent,
     FooterComponent,
@@ -16,7 +18,9 @@ import { RecipesComponent } from './recipes/recipes.component';
     RecipesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
